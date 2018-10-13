da834889253da7f730eb2981a32726673a65dc76
huanlui <jlgarcia@crcit.es>
2018-10-13 12:22:10 +0200

    Añado más tests sobre strings , sobre todo de interpolación

--- test_strings.py
+++ test_strings.py
@@ -30,5 +30,38 @@ def test_para_escoger_los_n_primeros_caracteres_hay_una_forma_comoda_que_funcion
     assert tresPrimerasLetrasTextoLargo == "abc"
     assert tresPrimerasLetrasTextoCorto == "ab"
 
+def test_hay_una_forma_mas_facil_para_quedarse_con_los_n_ultimos_caracteres_de_un_texto():
+    str = "abcde"
+    con_forma_de_toda_la_vida = str[len(str) - 2:]
+    con_forma_mejor = str[-2:]
 
+    assert con_forma_de_toda_la_vida == "de"
+    assert con_forma_mejor == "de"
+
+def test_permite_interpolacion_desde_version_3_6() :
+    nombre = "Paco"
+    edad = 76
+    saludo = f'Hola, soy {nombre}, y tengo sólo {edad} años'
+
+    assert saludo == "Hola, soy Paco, y tengo sólo 76 años"
+
+def test_como_en_javascript_se_puede_usar_comilla_doble_o_comilla_simple() :
+    nombreConComillasDobles = "Paco"
+    nombreConComillasSimples = 'Paco'
+
+    assert nombreConComillasDobles == nombreConComillasSimples
+
+def test_otra_forma_de_interpolar_algo_mas_incomoda_pero_compatible_con_versiones_anteriores() :
+    nombre = "Paco"
+    edad = 76
+    saludo = 'Hola, soy %s, y tengo sólo %s años'%(nombre,edad)
+
+    assert saludo == "Hola, soy Paco, y tengo sólo 76 años"
+
+def test_tambien_se_pueden_nombrar_los_parametros_para_repetir_o_desprecouparse_del_orden_de_los_parametros() :
+    nombre = "Paco"
+    edad = 76
+    saludo = 'Hola, soy {nombre}, sí {nombre}, y tengo sólo {edad} años'.format(edad=edad, nombre=nombre)
+
+    assert saludo == "Hola, soy Paco, sí Paco, y tengo sólo 76 años"
 
