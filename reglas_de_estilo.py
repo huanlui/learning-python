7ecfe11d39fe19a9b91a576a4231ef07aa17e768
huanlui <jlgarcia@crcit.es>
2018-11-04 13:27:34 +0100

    Subo resumen y enlaces de reglas de estilo

---
+++ reglas_de_estilo.py
@@ -0,0 +1,15 @@
+# Tocho: https://www.python.org/dev/peps/pep-0008/
+# Resumen: https://gist.github.com/RichardBronosky/454964087739a449da04
+# De Medium: https://medium.com/@dasagrivamanu/python-naming-conventions-the-10-points-you-should-know-149a9aa9f8c7 Bueno, pero sin ejenmplos
+
+"""
+En resumen:
+ - Nombres de funciones, tanto sueltas como de clase (métodos) en formato mi_funcion()
+ - Nombres de variables, tanto sueltas como de clase (campos) en formato mi_variable
+ - Nombres de paquetes y módulo, en formato mi_paquete. Cuanto más corto, mejor, y si es de una sola palabra, mejor 
+ - Constantes en formato MI_CONSTANTE.
+ - Nombres de clases en formato MiClase. 
+ - Si la clase es callable y esdtá bien documentada, se podría hacer con mi_clase(): https://stackoverflow.com/questions/111234/what-is-a-callable-in-python
+ - No pasa nada si una variable de un solo caracter lo ponemos en maúsculas: X. 
+ - Flake8 te ayuda a autocumplir esto: http://flake8.pycqa.org/en/latest/
+"""
